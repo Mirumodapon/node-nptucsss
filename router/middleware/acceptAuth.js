da76822c5dc0a3aa8057748e4ceaeeffdbbffc8d
mirumo <mysteryperson520@gmail.com>
2021-05-18 02:40:33 +0800

    [fix] middleware check token parse successed

--- router/middleware/acceptAuth.js
+++ router/middleware/acceptAuth.js
@@ -1,5 +1,6 @@
 module.exports = function (auth) {
     return (req, res, next) => {
+        if (auth === 'null') res.status(401).json('Authorization Denied');
         if (req.auth.authority <= auth) next();
         else res.status(401).json('Authorization Denied');
     }
